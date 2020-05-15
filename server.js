0550a712949993ecaa6e0c5dcf971e5a0b163e66
Jennifer Kruk <jenkruk@gmail.com>
2020-05-15 07:40:21 -0700

    clean up of unneeded comments

--- server.js
+++ server.js
@@ -18,36 +18,6 @@ var db = require("./models");
 
 // Import routes and give the server access to them.
 // var routes = require("./routes/routes.js");
-// app.use(routes);
-
-
-//Place Routes Here
-// app.get('/projects',(req,res)=>{ db.Project.findAll().then((data)=>{ res.json(data)})})
-
-// app.get('/projectManagers',(req,res)=>{ db.ProjectManager.findAll().then((data)=>{ res.json(data)})})
-
-//Find all items and include Departments
-// app.get('/itemsWithDepartments',(req,res)=>{ 
-//   db.Item.findAll({include: [db.Department]})
-//   .then((data)=>{ res.json(data)})
-// })
-
-
-//Finds add departments with Items included
-// app.get('/findAllDwithI',(req,res)=>{ 
-//   db.Department.findAll({include: [db.Item]})
-//   .then((data)=>{ res.json(data)})
-// })
-
-// Finds one department with Items included
-// app.get('/findOneDwithI/:dept',(req,res)=>{ 
-//   db.Department.findAll({
-//     where : {department : req.params.dept},
-//     include: [db.Item]
-//   })
-//   .then((data)=>{ res.json(data)})
-// })
-
 
 
 // Syncing our sequelize models and then starting our express app
