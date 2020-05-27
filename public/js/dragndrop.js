d4400429fbfa302403357f48b4c2256b9d4d4a9b
Jennifer Kruk <jenkruk@gmail.com>
2020-05-26 21:10:53 -0700

    drag and drop added to manager's page

--- public/js/dragndrop.js
+++ public/js/dragndrop.js
@@ -4,14 +4,13 @@
 $(document).ready(function() { 
 
     // for team member page
-
     $( "#waiting, #assigned, #active, #done" ).sortable({
         connectWith: ".sort",
         dropOnEmpty: false
     });
 
     // for manager page
-    $( "#project1, #project2, #project3, #project4").sortable({
+    $( "#available, #project1, #project2, #project3").sortable({
         connectWith: ".sort",
         dropOnEmpty: false
     });
