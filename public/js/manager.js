3130bf3663cd0ceb1dca33de7d25811d10e745a2
Jennifer Kruk <jenkruk@gmail.com>
2020-05-27 07:24:23 -0700

    updating master prior to peer review

--- public/js/manager.js
+++ public/js/manager.js
@@ -4,16 +4,16 @@ $("#add-form").hide();
 
 $(document).ready(function(){
 
-  $("#del").on('click',function() {
+  $("#del").on("click",function() {
     var li = $(this).parent();
     li.fadeOut(function(){
       li.remove();
     });
   });
 
-  $("#openForm").on('click', function() {
-    $("#openForm").hide();
-    $("#add-form").show();
+  $("#openForm").on("click", function() {
+  $("#openForm").hide();
+  $("#add-form").show();
 });
 
   $(".plus").on("click",function(e){
@@ -36,6 +36,27 @@ $(document).ready(function(){
     $("#add-form").hide();
     $("#openForm").show();
   });
+
+
+  $("#addProject").on("click",function(){
+
+      var project = $(
+        "<div class='card white col sm3 card-content blue-grey-text'><span class='card-title center'><h6>New Project</h6></span><hr><ol id='newProject' class='sort'><li>Jayne Doe<i class='tiny material-icons'id='del'>delete_forever</i></li></ol></div>"
+        ).text($(".txtb").val().trim());
+      var del = $("<i class='tiny material-icons'>delete_forever</i>").click(function(){
+        var card = $(this).parent();
+        card.fadeOut(function(){
+          card.remove();
+        });
+      });
+
+      $(".cardRow").append(project);
+      project.append(del);
+      
+
+  });
+
+
 
 
 }); //end document.ready
